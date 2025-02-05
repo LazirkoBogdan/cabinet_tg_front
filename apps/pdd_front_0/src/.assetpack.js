e213b6304167d90e720926d7043842cd42a65b32
bohdan <bohdan@spinach.games>
2025-02-05 18:29:11 +0200

    fix assets

--- apps/pdd_front_0/src/.assetpack.js
+++ apps/pdd_front_0/src/.assetpack.js
@@ -1,27 +1,23 @@
 import { AssetPack } from '@assetpack/core';
 
-
-
-import { compress } from '@assetpack/plugin-compress'
-import { pixiTexturePacker } from '@assetpack/plugin-texture-packer'
-import { ffmpeg } from '@assetpack/plugin-ffmpeg'
-import { json } from '@assetpack/plugin-json'
-import { pixiManifest } from '@assetpack/plugin-manifest'
-import { mipmap } from '@assetpack/plugin-mipmap'
-import { webfont } from '@assetpack/plugin-webfont'
+import { compress } from '@assetpack/plugin-compress';
+import { pixiTexturePacker } from '@assetpack/plugin-texture-packer';
+import { ffmpeg } from '@assetpack/plugin-ffmpeg';
+import { json } from '@assetpack/plugin-json';
+import { pixiManifest } from '@assetpack/plugin-manifest';
+import { mipmap } from '@assetpack/plugin-mipmap';
+import { webfont } from '@assetpack/plugin-webfont';
 
 const options = {
   jpg: {},
   png: { quality: 90 },
-  webp: { quality: 80, alphaQuality: 80, },
+  webp: { quality: 80, alphaQuality: 80 },
   avif: true,
   bc7: false,
   astc: false,
   basis: false,
-  etc: false
+  etc: false,
 };
-
-
 
 // "@assetpack/core": "^0.8.0",
 //   "@assetpack/plugin-compress": "^0.8.0",
@@ -32,27 +28,28 @@ const options = {
 //             "@assetpack/plugin-texture-packer": "^0.8.0",
 //               "@assetpack/plugin-webfont": "^0.5.1",
 
-
 const assetpack = new AssetPack({
   entry: './raw-assets',
   output: './assets',
   cache: true,
   cacheLocation: '.assetpack',
   logLevel: 'info',
-
+  //@ts-ignore
   plugins: [
-
+    //@ts-ignore
     compress(options),
+    //@ts-ignore
     pixiTexturePacker({
       texturePacker: {
         padding: 2,
-        nameStyle: "relative",
+        //@ts-ignore
+        nameStyle: 'relative',
         removeFileExtension: false,
       },
       resolutionOptions: {
-        template: "@%%x",
+        template: '@%%x',
         resolutions: { default: 1, low: 0.5 },
-        fixedResolution: "default",
+        fixedResolution: 'default',
         maximumTextureSize: 4096,
       },
     }),
@@ -80,20 +77,15 @@ const assetpack = new AssetPack({
       ],
     }),
 
-    pixiManifest(
-      {
-        output: "./assets/manifest.json",
-        createShortcuts: false,
-        trimExtensions: false,
-        includeMetaData: true,
-        nameStyle: 'short'
-      }
-    ),
-
+    pixiManifest({
+      output: './assets/manifest.json',
+      createShortcuts: false,
+      trimExtensions: false,
+      //@ts-ignore
+      includeMetaData: true,
+      nameStyle: 'short',
+    }),
   ],
-
 });
 
-
 await assetpack.run();
-
