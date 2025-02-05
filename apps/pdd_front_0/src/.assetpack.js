bf43746cb35cb7ad0c6d22435241bef0733f7b7d
bohdan <bohdan@spinach.games>
2025-02-05 18:32:25 +0200

    merge

---
+++ apps/pdd_front_0/src/.assetpack.js
@@ -0,0 +1,99 @@
+import { AssetPack } from '@assetpack/core';
+
+
+
+import { compress } from '@assetpack/plugin-compress'
+import { pixiTexturePacker } from '@assetpack/plugin-texture-packer'
+import { ffmpeg } from '@assetpack/plugin-ffmpeg'
+import { json } from '@assetpack/plugin-json'
+import { pixiManifest } from '@assetpack/plugin-manifest'
+import { mipmap } from '@assetpack/plugin-mipmap'
+import { webfont } from '@assetpack/plugin-webfont'
+
+const options = {
+  jpg: {},
+  png: { quality: 90 },
+  webp: { quality: 80, alphaQuality: 80, },
+  avif: true,
+  bc7: false,
+  astc: false,
+  basis: false,
+  etc: false
+};
+
+
+
+// "@assetpack/core": "^0.8.0",
+//   "@assetpack/plugin-compress": "^0.8.0",
+//     "@assetpack/plugin-ffmpeg": "^0.8.0",
+//       "@assetpack/plugin-json": "^0.8.0",
+//         "@assetpack/plugin-manifest": "^0.8.0",
+//           "@assetpack/plugin-mipmap": "^0.8.0",
+//             "@assetpack/plugin-texture-packer": "^0.8.0",
+//               "@assetpack/plugin-webfont": "^0.5.1",
+
+
+const assetpack = new AssetPack({
+  entry: './raw-assets',
+  output: './assets',
+  cache: true,
+  cacheLocation: '.assetpack',
+  logLevel: 'info',
+
+  plugins: [
+
+    compress(options),
+    pixiTexturePacker({
+      texturePacker: {
+        padding: 2,
+        nameStyle: "relative",
+        removeFileExtension: false,
+      },
+      resolutionOptions: {
+        template: "@%%x",
+        resolutions: { default: 1, low: 0.5 },
+        fixedResolution: "default",
+        maximumTextureSize: 4096,
+      },
+    }),
+    ffmpeg({
+      inputs: ['.mp3', '.ogg', '.wav'],
+      outputs: [
+        {
+          formats: ['.mp3'],
+          recompress: false,
+          options: {
+            audioBitrate: 96,
+            audioChannels: 1,
+            audioFrequency: 48000,
+          },
+        },
+        {
+          formats: ['.ogg'],
+          recompress: false,
+          options: {
+            audioBitrate: 32,
+            audioChannels: 1,
+            audioFrequency: 22050,
+          },
+        },
+      ],
+    }),
+
+    pixiManifest(
+      {
+        output: "./assets/manifest.json",
+        createShortcuts: false,
+        trimExtensions: false,
+        includeMetaData: true,
+        nameStyle: 'short'
+      }
+    ),
+
+  ],
+
+});
+
+
+await assetpack.run();
+
